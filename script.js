f72ca56605a3c12f99d956e744a72b0949b4e5df
codetwentyfive <chingisenkhbaatar@gmail.com>
2023-07-20 04:22:36 +0200

    built book render function and remove book function

--- script.js
+++ script.js
@@ -9,9 +9,28 @@ function Book(title, author, pages, read) {
 }
 
 function render() {
-  let libraryBook = document.querySelector()
-}
+  let bookDisplay = document.querySelector(".bookDisplay");
+  bookDisplay.innerHTML = ""; // empties the html 
+  for (let i = 0; i < myLibrary.length; i++) {
+    let book = myLibrary[i];
+    let bookEl = document.createElement("div");
+    bookEl.innerHTML = `<div class="book">
+                    <h2 class="title">${book.title}</h2>
+                    <p class="author">By: <${book.author}/p>
+                    <p class="pages">Pages:${book.pages}304</p>
+                    <p class "read-status">${book.read ? "Read" : "Not read yet"}</p>
+                    <button class="removeBtn" onclick="removeBook(${i})">Remove</button>
+                    </div>`;
+    bookDisplay.appendChild(bookEl);
+  }
 
+
+
+}
+function removeBook(index) {
+  myLibrary.splice(index, 1);
+  render();
+}
 function addBookToLibrary() {
 
   let title = document.querySelector("#title").value;
@@ -22,7 +41,7 @@ function addBookToLibrary() {
   let newBook = new Book(title, author, pages, read);
 
   myLibrary.push(newBook);
-  render():
+  render();
 
 
 }
@@ -33,7 +52,7 @@ let newBookBtn = document.querySelector("#new-book-btn");
 
 newBookBtn.addEventListener("click", function () {
   let newBookForm = document.querySelector("#new-book-form");
-  newBookForm.style.display = "block";
+  newBookForm.style.display = "flex";
 
 })
 
